ab17969aa7a5372c21ca59d9e9de08d45bcc26ba
Prateek2593 <patidarprateek2593@gmail.com>
2026-01-09 17:27:27 +0530

    working with outputs and emitting data

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component, Input, computed, input} from '@angular/core';
+import { Component, EventEmitter, Input, Output} from '@angular/core';
 
 import { DUMMY_USERS } from '../dummy_users';
 
@@ -10,15 +10,16 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.css',
 })
 export class User {
-  // @Input({required:true}) avatar! : string;
-  // @Input({required:true}) name! : string;
+  @Input({required:true}) id! : string;
+  @Input({required:true}) avatar! : string;
+  @Input({required:true}) name! : string;
 
-  avatar = input.required<string>();
-  name = input.required<string>();
+  @Output() select = new EventEmitter();
 
-  // get imagePath(){
-  //   return 'assets/users/' + this.avatar;
-  // }
-  imagePath = computed(() => 'assets/users/' + this.avatar())
-  onSelectUser() {}
+  get imagePath(){
+    return 'assets/users/' + this.avatar;
+  }
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
 }
