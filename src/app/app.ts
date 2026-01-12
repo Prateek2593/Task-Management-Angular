c003d17b5a16a2b93ea0b36fe173b5b365b85104
Prateek2593 <patidarprateek2593@gmail.com>
2026-01-12 10:07:20 +0530

    printing selected user name in tasks area

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,25 @@
 import { Component } from '@angular/core';
 import { Header } from './header/header';
 import { User } from './user/user';
+import { Tasks } from './tasks/tasks';
 import { DUMMY_USERS } from './dummy_users';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, User],
+  imports: [Header, User, Tasks],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class AppComponent {
   users = DUMMY_USERS;
 
+  selectedUserId = 'u1';
+
+  get selectedUser(){
+    return this.users.find((user) =>user.id === this.selectedUserId)!;
+  }
   onSelectUser(id:string){
-    console.log("Selected user with id: ", id);
+    this.selectedUserId = id;
   }
 }
