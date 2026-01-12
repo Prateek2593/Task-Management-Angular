c73d2b313d60694a93c771e75c1b0ba7da5a4119
Prateek2593 <patidarprateek2593@gmail.com>
2026-01-12 15:28:22 +0530

    creating new component

--- src/app/tasks/tasks.ts
+++ src/app/tasks/tasks.ts
@@ -1,16 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy_users';
 import { Task } from './task/task';
+import { NewTask } from './new-task/new-task';
 
 @Component({
   selector: 'app-tasks',
-  imports: [Task],
+  imports: [Task, NewTask],
   templateUrl: './tasks.html',
   styleUrl: './tasks.css',
 })
 export class Tasks {
   @Input({required:true}) name ?: string;
   @Input({required:true}) userId!: string;
+  isAddingTask = false;
 
   tasks = [
     {
@@ -44,5 +46,9 @@ export class Tasks {
 
   onCompleteTask(id : string){
     this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask(){
+    this.isAddingTask = true;
   }
 }
